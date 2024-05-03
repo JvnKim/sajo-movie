f9622f1406d48c76acd9558fb9df03d263fae825
host2024 <youwoon5520@gmail.com>
2024-05-03 17:25:09 +0900

    2024.5.3 운성 메인 메뉴 넣기

--- src/fetch.js
+++ src/fetch.js
@@ -1,17 +1,13 @@
-import {
-  display_JenreButtons,
-  display_Movies,
-  display_Spinner,
-} from "./render.js";
+import { display_JenreButtons, display_Movies, display_Spinner } from './render.js';
 
-const TBDB_URL = "https://api.themoviedb.org/3";
+const TBDB_URL = 'https://api.themoviedb.org/3';
 const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWZmNmMxY2VhMDUxMTg3ZTYzMTUzYWQyYzYzY2E5ZCIsInN1YiI6IjY0ZTg1ZmU2NTI1OGFlMDE0ZGYyMjZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LwbMg29luCKo58wfbjsZRD8NXPMkXj3CRD-WwwzGeTk",
-  },
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OWZmNmMxY2VhMDUxMTg3ZTYzMTUzYWQyYzYzY2E5ZCIsInN1YiI6IjY0ZTg1ZmU2NTI1OGFlMDE0ZGYyMjZlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LwbMg29luCKo58wfbjsZRD8NXPMkXj3CRD-WwwzGeTk',
+    },
 };
 
 /*
@@ -19,67 +15,137 @@ const options = {
 */
 // 배우 불러오는 함수
 
-
-
 // 초기 영화 목록을 불러오는 함수
+
+// 2024.5.3 운성 수정
 export async function fetch_Movies() {
-  display_Spinner(true); // 스피너 실행
-  await fetch(`${TBDB_URL}/movie/now_playing?language=ko-KR&page=1`, options)
-    .then((response) => response.json())
-    .then((data) => {
-      display_Spinner(false); // 데이터를 가져왔으면 스피너 숨김
-      const movies = data.results; // 영화 목록을 저장
-      // 영화 출력해주기
-      display_Movies(movies, document.getElementById("movieList")); // render.js 함수
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    display_Spinner(true);
+    try {
+        const response = await fetch(`${TBDB_URL}/movie/now_playing?language=ko-KR&page=1`, options);
+        const data = await response.json();
+        display_Spinner(false);
+        display_Movies(data.results, document.getElementById('movieList')); // 영화 목록을 화면에 표시
+        updateSliderWithMovies(data.results); // 슬라이더 업데이트
+    } catch (error) {
+        console.error('Error fetching movies:', error);
+        display_Spinner(false);
+    }
 }
+
+async function fetchMovieTrailers(movieId) {
+    const trailerOptions = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTUxMjBmOGFmMWJkNmQ0OGU2Y2E4MDAzYmY2ZjAwNiIsInN1YiI6IjY2MjYzNjVkN2E5N2FiMDE3ZDhmMTc3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y0YvIb-StdqiAR5fgypwYHkV8MLZhbAJ5ygnX7ebCcY',
+        },
+    };
+
+    try {
+        const response = await fetch(`${TBDB_URL}/movie/${movieId}/videos?language=en-US`, trailerOptions);
+        const data = await response.json();
+        const trailer = data.results.find((video) => video.type === 'Trailer' && video.site === 'YouTube');
+        return trailer ? trailer.key : null; // YouTube video key if trailer exists
+    } catch (error) {
+        console.error('Error fetching trailers:', error);
+        return null;
+    }
+}
+
+async function updateSliderWithMovies(movies) {
+    const slider = document.getElementById('movieSlider');
+    slider.innerHTML = ''; // 슬라이더 내용 초기화
+
+    for (const movie of movies) {
+        const trailerKey = await fetchMovieTrailers(movie.id); // 트레일러 키 가져오기
+        const trailerEmbedUrl = trailerKey ? `https://www.youtube.com/embed/${trailerKey}?autoplay=0&mute=1` : '';
+
+        const slide = document.createElement('div');
+        slide.className = 'slide';
+        slide.style.display = 'none';
+        // 트레일러가 있으면 임베드하고, 없으면 메시지 표시
+        const iframePart = trailerKey
+            ? `<iframe src="${trailerEmbedUrl}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="custom-iframe"></iframe>`
+            : `<div>트레일러가 없습니다</div>`;
+
+        slide.innerHTML = `
+          <div class="slide-content">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="slide-img">
+              <div class="movie-info">
+              ${iframePart}
+                  <h3>${movie.title}</h3>
+                  <p>개봉일: ${movie.release_date}</p>
+                  <p>평점: ${movie.vote_average} / 10</p>
+                  
+              </div>
+          </div>
+      `;
+        slider.appendChild(slide);
+    }
+
+    startSlider(); // 슬라이더 애니메이션 시작
+}
+
+function startSlider() {
+    let slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+    if (slides.length > 0) {
+        slides[currentIndex].style.display = 'block'; // 첫 번째 슬라이드 표시
+
+        setInterval(() => {
+            slides[currentIndex].style.display = 'none'; // 현재 슬라이드 숨김
+            currentIndex = (currentIndex + 1) % slides.length;
+            slides[currentIndex].style.display = 'block'; // 다음 슬라이드 표시
+        }, 180000); // 3분마다 슬라이드 변경
+    }
+}
+
+// 운성
 
 // 장르 목록을 불러오는 함수
 export async function fetch_Jenres() {
-  await fetch(`${TBDB_URL}/genre/movie/list?language=en`, options)
-    .then((response) => response.json())
-    .then((data) => {
-      const jenres = new Map(data.genres.map((item) => [item.name, item.id]));
-      // 장르 버튼 출력하는 함수
-      display_JenreButtons(jenres); // render.js 함수
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    await fetch(`${TBDB_URL}/genre/movie/list?language=en`, options)
+        .then((response) => response.json())
+        .then((data) => {
+            const jenres = new Map(data.genres.map((item) => [item.name, item.id]));
+            // 장르 버튼 출력하는 함수
+            display_JenreButtons(jenres); // render.js 함수
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 // 검색 결과 가져오기 (수정 및 테스트 완료)
 export async function fetch_SearchMovies(keyword) {
-  await fetch(
-    `${TBDB_URL}/search/movie?query=${keyword}&language=ko-KR&page=1&include_adult=false`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let filteredMovies = data.results;
+    try {
+        const response = await fetch(
+            `${TBDB_URL}/search/movie?query=${encodeURIComponent(keyword)}&language=ko-KR&page=1&include_adult=false`,
+            options
+        );
+        const data = await response.json();
+        let filteredMovies = data.results;
 
-      // 영화 출력해주기
-      display_Movies(filteredMovies); // render.js 함수
-    })
-    .catch((err) => console.error(err));
+        // 영화 출력해주기
+        display_Movies(filteredMovies); // render.js 함수
+        return filteredMovies; // 검색된 영화 데이터를 반환
+    } catch (err) {
+        console.error(err);
+        throw err; // 에러를 호출자에게 전파
+    }
 }
 
 // 장르에 따른 영화 목록을 찾아서 불러오는 함수
 export async function fetch_SearchByJenre(genreId) {
-  display_Spinner(true);
-  await fetch(
-    `${TBDB_URL}/discover/movie?with_genres=${genreId}&language=ko-KR&page=1`,
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const movies = data.results;
-      // 영화 출력해주기
-      display_Movies(movies, document.getElementById("movieList")); // render.js 함수 파일
-      display_Spinner(false);
-    })
-    .catch((err) => console.error(err));
+    display_Spinner(true);
+    await fetch(`${TBDB_URL}/discover/movie?with_genres=${genreId}&language=ko-KR&page=1`, options)
+        .then((response) => response.json())
+        .then((data) => {
+            const movies = data.results;
+            // 영화 출력해주기
+            display_Movies(movies, document.getElementById('movieList')); // render.js 함수 파일
+            display_Spinner(false);
+        })
+        .catch((err) => console.error(err));
 }
--- src/main.js
+++ src/main.js
@@ -28,21 +28,47 @@
 // // 영화 카드 클릭 이벤트 처리 함수 호출 (2024년 5월1일 주현우 작성)
 // handleMovieCardClick();
 
-
-
-import { handleSearch, handleMovieCardClick } from "./eventListener.js";
-import { fetch_Jenres, fetch_Movies } from "./fetch.js";
+import { handleSearch, handleMovieCardClick } from './eventListener.js';
+import { fetch_Jenres, fetch_Movies } from './fetch.js';
 // import { handleMovieCardClick } from "./eventListener.js"; //주현우 작성
-import "./detail.js"; // 전은겸 작성
+import './detail.js'; // 전은겸 작성
 
+// 2024.5.3 운성 수정
+
+function updateSliderWithMovies(movies) {
+    const slider = document.getElementById('movieSlider');
+    slider.innerHTML = '';
+
+    movies.forEach((movie) => {
+        console.log('Movie data:', movie); // API에서 받은 데이터 확인
+        const genres = movie.genres.map((genre) => genre.name).join(', ');
+        const overview = movie.overview || '정보 없음';
+
+        const slide = document.createElement('div');
+        slide.className = 'slide';
+        slide.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" class="slide-img">
+          <div class="movie-info">
+              <h3>${movie.title}</h3>
+              <p>개봉일: ${movie.release_date}</p>
+              <p>평점: ${movie.vote_average}/10</p>
+              <p>장르: ${genres}</p>
+              <p>요약: ${overview}</p>
+          </div>
+      `;
+        slider.appendChild(slide);
+    });
+}
+
+// 운성
 
 //리다이렉트 함수
 function handleRedirect(container) {
-  document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(container).addEventListener("click", function () {
-      location.reload(); // 현재 페이지를 새로고침합니다.
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelector(container).addEventListener('click', function () {
+            location.reload(); // 현재 페이지를 새로고침합니다.
+        });
     });
-  });
 }
 
 //리다이렉트 함수
@@ -54,6 +80,6 @@ fetch_Jenres();
 //영화 정보 불러오기
 fetch_Movies();
 
-handleRedirect("h1");
+handleRedirect('h1');
 
 handleMovieCardClick(); // 수정된 부분
